66d74f770c289c85621833857c5de21051627b55
Cody Hoffman <cody-hoffman@uiowa.edu>
2017-03-02 18:00:50 -0600

    Completed Songs.java with constructors, getter and setters, toString

--- src/java/model/Songs.java
+++ src/java/model/Songs.java
@@ -24,5 +24,50 @@ public class Songs {
         this.songLength = songLength;
         this.songGenre = songGenre;
     }
+
+    public int getSongID() {
+        return songID;
+    }
+
+    public void setSongID(int songID) {
+        this.songID = songID;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public void setSongLength(int songLength) {
+        this.songLength = songLength;
+    }
+
+    public String getSongGenre() {
+        return songGenre;
+    }
+
+    public void setSongGenre(String songGenre) {
+        this.songGenre = songGenre;
+    }
+
+    @Override
+    public String toString() {
+        return "Songs{" + "songID=" + songID + ", songName=" + songName + ", artistName=" + artistName + ", songLength=" + songLength + ", songGenre=" + songGenre + '}';
+    }
     
 }
